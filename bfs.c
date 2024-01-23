6ab2a794fa859ed4b36f70a4f22807f575c08276
CHAKRIKA-SAI-D <120378173+CHAKRIKA-SAI-D@users.noreply.github.com>
2024-01-23 19:30:43 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,72 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
 
-// creating queue data structure using arrays
 int queue[100];
+int front, rear = 0;
 
-// defining pointers of the queue to perform pop and push
-int front=0,back=0;
-
-// defining push operation on the queue
-void push(int var)
-{
-    queue[back] = var;
-    back++;
+void push(int x) {
+    queue[rear] = x;
+    rear++;
 }
 
-// defining pop operation on queue
-void pop()
-{
+void pop() {
     queue[front] = 0;
     front++;
 }
 
-// creating a visited array to keep the track of visited nodes
-int visited[7] = {0};
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
 
-int main()
-{
-    // defining the number of total persons 
-    int N  = 6;
+    int visited[n];
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
 
-    // adjacenty matrix representing graph
-    int graph[6][6] = {{0,1,1,0,0,0},
-                     {1,0,1,0,0,0},
-                     {1,1,0,1,1,0},
-                     {0,0,1,0,0,0},
-                     {0,0,1,0,0,1},
-                     {0,0,0,0,1,0}};
-    
-    // front == back stands for empty queue 
-    // until queue is not empty perfroming bfs 
+    printf("Enter the matrix : ");
+    int graph[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
 
-
-    // adding a starting node in the list 
     push(1);
-    visited[0] = 1; // marking 1st node as visited 
-    while(front != back)
-    {
+    visited[0] = 1;
+
+    while (front != rear) {
         int current = queue[front];
-        
-        // printing current element 
-        printf("%d ", current);
-        
-        // popping the front element from the queue
         pop();
- 
-        for(int i=0;i<6;i++)
-        {
-            // adding non-visited connected nodes of the current node to the queue 
-            if((graph[current-1][i] == 1) && (visited[i] == 0))
-            {
-                visited[i] = 1; // marking visisted
-                push(i+1);
+        printf("%d ", current); // Print the visited node
+
+        for (int i = 0; i < n; i++) {
+            if ((graph[current - 1][i] == 1) && (visited[i] == 0)) {
+                visited[i] = 1;
+                push(i + 1);
             }
         }
     }
+
     return 0;
 }
 
 
+
 -----------------------------------------------------------------------------------------------------------------------------------
   #output
-  1 2 3 4 5 6 
+Enter size: 3
+Enter the matrix : 0 1 0
+1 0 1
+0 1 0
+0 1 0
+
+1 0 1
+
+0 1 0
+1 2 3 
